7349d6af338ce1cc6cae8682d015be418603fdae
NathanDeas <nathanmd0312@outlook.com>
2026-01-20 20:30:28 -0500

    Carousel client logos

--- HaulPointsFE/src/Homepage/components/Partners.jsx
+++ HaulPointsFE/src/Homepage/components/Partners.jsx
@@ -2,8 +2,26 @@ import "../css/Partners.css"
 function Partners() {
     return (
         <div className="partners-container">
-
-            
+            <div className="carousel">
+                <div className="icon-wrapper">
+                    <div className="icon-container">
+                        <img src="/Company Logos/BORCELLE.png" />
+                        <img src="/Company Logos/FIRELINE.png" />
+                        <img src="/Company Logos/HANOVER.png" />
+                        <img src="/Company Logos/MIDCOAST.png" />
+                        <img src="/Company Logos/REDRIVER.png" />
+                        <img src="/Company Logos/SUNBELT.png" />
+                    </div>
+                    <div aria-hidden className="icon-container-copy">
+                        <img src="/Company Logos/BORCELLE.png" />
+                        <img src="/Company Logos/FIRELINE.png" />
+                        <img src="/Company Logos/HANOVER.png" />
+                        <img src="/Company Logos/MIDCOAST.png" />
+                        <img src="/Company Logos/REDRIVER.png" />
+                        <img src="/Company Logos/SUNBELT.png" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
