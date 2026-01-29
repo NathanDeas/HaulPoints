0a02966fb32a740492a93840a665137101380fb6
NathanDeas <nathanmd0312@outlook.com>
2026-01-28 23:50:38 -0500

    how it works section flow/style/icons update, intersection observer for header styling change on scroll, other minor styling fixes and updates

--- HaulPointsFE/src/Header/components/Header.jsx
+++ HaulPointsFE/src/Header/components/Header.jsx
@@ -1,18 +1,22 @@
 import {react} from 'react';
 import { NavLink } from 'react-router-dom';
 import '../css/Header.css';
+import { useInView } from 'react-intersection-observer';
 
-function Header() {
+function Header({headerActionRef}) {
+
   return (
-    <div className="header-container">
-      <img src='./HaulPointsLogo-BlackText.png' className='logo'></img>
-      <nav className='nav-buttons'>
-        <span className="nav-link">About</span>
-        <span className="nav-link">Partners</span>
-        <span className="nav-link">FAQ</span>
-        <span className="nav-link">Contact</span>
-        <NavLink className="link-to-login" to="/login">Login</NavLink>
-      </nav>
+    <div ref = {headerActionRef} className="header-container">
+      <div className='header-content-wrapper'>
+        <img src='./HaulPointsLogo-White.png' className='logo'></img>
+        <nav className='nav-buttons'>
+          <span className="nav-link">About</span>
+          <span className="nav-link">Partners</span>
+          <span className="nav-link">FAQ</span>
+          <span className="nav-link">Contact</span>
+          <NavLink className="nav-link to-login" to="/login">Login</NavLink>
+        </nav>
+      </div>
     </div>
 
     );
--- HaulPointsFE/src/Homepage/Pages/Homepage.jsx
+++ HaulPointsFE/src/Homepage/Pages/Homepage.jsx
@@ -1,20 +1,39 @@
-import React from "react";
 import "../css/homepage.css"
 import Header from "../../Header/components/Header.jsx"
 import HeroBanner from "../components/banner.jsx"
 import HowItWorks from "../components/HowItWorks.jsx"
 import Partners from "../components/Partners.jsx"
+import { useRef, useEffect } from "react"
+// import Testimonials from "../components/Testimonials.jsx";
 
 
 
 function Homepage() {
+  const headerScrollTarget = useRef();
+  const headerActionRef = useRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      console.log(headerActionRef.current);
+      if(!entries[0].isIntersecting) {
+        headerActionRef.current.classList.add("scrolled")
+      }
+      else {
+        headerActionRef.current.classList.remove("scrolled")
+      }
+    });
+    observer.observe(headerScrollTarget.current);
+    return () => observer.disconnect();
+
+  }, [])
 
   return (  
     <>
-    <Header />
+    <div ref={headerScrollTarget} className="header-scroll-trigger"></div>
+    <Header headerActionRef = {headerActionRef}/>
     <div className="homepage-container">
       <HeroBanner />
-      <HowItWorks />
+      < HowItWorks />
+      {/* <Testimonials /> */}
       <Partners />
     </div>
     </>
