b30b7cf182ff2cf327c3f54fde00a1daf9b74995
NathanDeas <nathanmd0312@outlook.com>
2026-02-10 23:35:51 -0500

    auth pages responsive for mobile layouts

--- HaulPointsFE/src/users/components/AuthLayout.jsx
+++ HaulPointsFE/src/users/components/AuthLayout.jsx
@@ -4,8 +4,10 @@ import { NavLink } from "react-router-dom";
 function AuthLayout  ({ children }) {
     return (
         <div className="auth-page-layout">
+            <div className="logo-wrapper">
+                <img className="logo" src="/HaulPointsLogo-BlackText.png"/>
+            </div>
             <div className="page-left-img">
-                <img className="logo" src="/HaulPointsLogo-BlackText.png"></img>
                 <img className="stock-truck-image" src="/Loginregister-image-left.jpg"></img>
             </div>
             <div className="page-right-forms">
